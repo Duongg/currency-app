2ebc44be4dc3b5496b19a498fa56941c1e90c8d0
Duongg <ducduong2201@gmail.com>
2024-09-18 00:07:18 +0700

    bump version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
         applicationId = "org.example.currencyapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
     }
     packaging {
         resources {
